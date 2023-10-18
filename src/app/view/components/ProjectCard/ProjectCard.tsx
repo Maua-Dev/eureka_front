b60584337889bbd8c8d852b455379a18654be3a2
EnricoSantarelli <enricosantarelli@hotmail.com>
2023-10-18 06:55:49 -0300

    increase font tables

--- src/app/view/components/ProjectCard/ProjectCard.tsx
+++ src/app/view/components/ProjectCard/ProjectCard.tsx
@@ -5,20 +5,36 @@ interface ProjectCardProps {
   image: string;
   title: string;
   teacherAdvisor: string;
+  newDeliveries?: string[];
 }
 
 export default function ProjectCard({
   image,
   title,
   teacherAdvisor,
+  newDeliveries,
 }: ProjectCardProps) {
   return (
-    <Link id="card" to={'/project'}>
-      
-        
-      <div className="image"
+    <Link id="card" to={"/project"}>
+      <div className="popup">
+        <div className="circle ">
+          <div className="bubble">
+            <h1>Nova entrega em: </h1>
+            <ul>
+              {newDeliveries?.map((delivery, index) => (
+                <li key={index}>{delivery}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <span>Novo!</span>
+      </div>
+
+      <div
+        className="image"
         style={{
-          backgroundImage: `url(${image})`, backgroundSize: 'cover'
+          backgroundImage: `url(${image})`,
+          backgroundSize: "cover",
         }}
       />
       <aside>
