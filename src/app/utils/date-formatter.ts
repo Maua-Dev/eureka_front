319b4eebf16c60fe41fe3448b3ea1adfe228225a
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-02-06 13:26:28 -0300

    update date formatter

--- src/app/utils/date-formatter.ts
+++ src/app/utils/date-formatter.ts
@@ -1,13 +1,4 @@
-// this method returns a date as a string formatted as DD/MM/YYYY
-export function formatDate(date: Date) : string{
-    return [
-        date.getDate().toString().padStart(2, "0"),
-        (date.getMonth() + 1).toString().padStart(2, "0"),
-        date.getFullYear(),
-      ].join("/");
-}
-
 // this method returns a date from a string formatted as DD/MM/YYYY
-export function formatString(string: string): Date {
+export function stringToDate(string: string): Date {
   return new Date(string.slice(3, 5).concat("/", string.slice(0, 2), "/", string.slice(6, 10)));
 }
