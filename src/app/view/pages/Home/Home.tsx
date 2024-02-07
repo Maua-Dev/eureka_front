96da7ffde178cd5bd98b24ca9d6a39f8236d22bc
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-02-06 21:22:27 -0300

    add card from get projects request

--- src/app/view/pages/Home/Home.tsx
+++ src/app/view/pages/Home/Home.tsx
@@ -2,8 +2,12 @@ import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import workingImage from "../../../assets/working-image.jpg";
 import arrowIcon from "../../../assets/icons/arrow-icon.svg";
 import "./Home.css";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Dialog from "../../components/Dialog/Dialog";
+import { ProjectContext } from "../../../context/project-context";
+import { AuthContext } from "../../../context/auth-context";
+import { ROLE } from "../../../../@clean/shared/domain/enums/role-enum";
+import { stringCapitalize } from "../../../utils/string-formatter";
 
 export default function Home() {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
@@ -11,61 +15,72 @@ export default function Home() {
     "Visualizar trabalhos como"
   );
 
-  return (
-    <>
-      <main id="home">
-        <div className="container">
-          <Dialog setOpen={setIsDropdownOpen} className="dropdown">
-            <button
-              className="dropdown__button"
-              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-            >
-              <p className="dropdown__text">{dropdownText}</p>
-              <img
-                className="dropdown__icon"
-                src={arrowIcon}
-                style={isDropdownOpen ? { transform: "scale(1, -1)" } : {}}
-                alt="Ícone de flecha"
-              />
-              {isDropdownOpen && <section className="dropdown__options">
-                <div
-                  className="option"
-                  onClick={(event) => {
-                    setDropdownText(event.currentTarget.innerHTML);
-                  }}
-                >
-                  Professor orientador
-                </div>
-                <div
-                  className="option"
-                  onClick={(event) => {
-                    setDropdownText(event.currentTarget.innerHTML);
-                  }}
-                >
-                  Professor responsável
-                </div>
-              </section>}
+  const { projects, getProjectsByRole } = useContext(ProjectContext);
+  const { user } = useContext(AuthContext);
 
-            </button>
-          </Dialog>
-        </div>
-        <ProjectCard
-          image={workingImage}
-          teacherAdvisor="Ana Paula"
-          title="DSG - Design universal aplicado em embalagem de protetor solar"
-          newDeliveries={["Dados do trabalho", "Pôster de imagem", "Dados do trabalho", "Pôster de imagem"]}
-        />
-        <ProjectCard
-          image={workingImage}
-          teacherAdvisor="Ana Paula"
-          title="DSG - Design universal aplicado em embalagem de protetor solar"
-        />
-        <ProjectCard
-          image={workingImage}
-          teacherAdvisor="Ana Paula"
-          title="DSG - Design universal aplicado em embalagem de protetor solar"
-        />
-      </main >
-    </>
+  useState(() => {
+    getProjectsByRole(user!.userId);
+  });
+
+  return (
+    <main id="home">
+      <div className="container">
+        <Dialog setOpen={setIsDropdownOpen} className="dropdown">
+          <button
+            className="dropdown__button"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
+            <p className="dropdown__text">{dropdownText}</p>
+            <img
+              className="dropdown__icon"
+              src={arrowIcon}
+              style={isDropdownOpen ? { transform: "scale(1, -1)" } : {}}
+              alt="Ícone de flecha"
+            />
+            {isDropdownOpen && <section className="dropdown__options">
+              <div
+                className="option"
+                onClick={(event) => {
+                  setDropdownText(event.currentTarget.innerHTML);
+                }}
+              >
+                Professor orientador
+              </div>
+              <div
+                className="option"
+                onClick={(event) => {
+                  setDropdownText(event.currentTarget.innerHTML);
+                }}
+              >
+                Professor responsável
+              </div>
+            </section>}
+
+          </button>
+        </Dialog>
+      </div>
+      {projects.map((project) => {
+        return (
+          <ProjectCard
+            key={project.projectId}
+            image={workingImage}
+            teacherAdvisor={stringCapitalize(project.professors.filter((professor) => professor.role === ROLE.ADVISOR)[0].name)}
+            title={project.title}
+            newDeliveries={["Dados do trabalho", "Pôster de imagem", "Dados do trabalho", "Pôster de imagem"]}
+          />
+        );
+
+      })}
+      <ProjectCard
+        image={workingImage}
+        teacherAdvisor="Ana Paula"
+        title="DSG - Design universal aplicado em embalagem de protetor solar"
+      />
+      <ProjectCard
+        image={workingImage}
+        teacherAdvisor="Ana Paula"
+        title="DSG - Design universal aplicado em embalagem de protetor solar"
+      />
+    </main >
   );
 }
