8ad2ccd31bdc9f6fdfb56e5c05c11f92174a57a0
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-02-06 21:21:10 -0300

    create mock jsons

---
+++ src/@clean/shared/infra/jsons/user-json.ts
@@ -0,0 +1,67 @@
+import { UserJsonProps } from "../../domain/entities/user";
+
+export class UserJson {
+    
+    static userJson : UserJsonProps[] = [
+        {
+            "user_id": 1,
+            "name": "CARLOS EDUARDO DANTAS DE MENEZES",
+            "email": "carlos.menezes@maua.br",
+            "role": "ADVISOR"
+        },
+        {
+            "user_id": 2,
+            "name": "ANA PAULA GONCALVES SERRA",
+            "email": "ana.serra@maua.br",
+            "role": "RESPONSIBLE"
+        },
+        {
+            "user_id": 3,
+            "name": "JOSÉ CAMARGO FONSECA",
+            "email": "jose.camargo@maua.br",
+            "role": "ADVISOR"
+        },
+        {
+            "user_id": 4,
+            "name": "MANOEL GOMES DA SILVA",
+            "email": "manuel.gomes@maua.br",
+            "role": "RESPONSIBLE"
+        },
+        {
+            "user_id": 5,
+            "name": "MARCOS DE PAULA SILVEIRA",
+            "email": "marcos.paula@maua.br",
+            "role": "ADVISOR"
+        },
+        {
+            "user_id": 6,
+            "name": "MANUELA PEREIRA SANTOS",
+            "email": "manuela.santos@maua.br",
+            "role": "RESPONSIBLE"
+        },
+        {
+            "user_id": 7,
+            "name": "MARIA DE FATIMA SILVA",
+            "email": "maria.fatima@maua.br",
+            "role": "STUDENT"
+        },
+        {
+            "user_id": 8,
+            "name": "GABRIEL SIQUEIRA NUNES",
+            "email": "gabriel.siqueira@maua.br",
+            "role": "STUDENT"
+        },
+        {
+            "user_id": 9,
+            "name": "FERNANDO HENRIQUE LIMA",
+            "email": "fernando.lima@maua.br",
+            "role": "STUDENT"
+        },
+        {
+            "user_id": 10,
+            "name": "Enrico Mota Santarelli",
+            "email": "enrico.santarelli@maua.br",
+            "role": "STUDENT"
+        },
+    ];
+}
