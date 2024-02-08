f1559bc86fe6767b8d4b09a9c9e0fcff9e22e7b0
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-02-07 21:25:55 -0300

    fix get project usecase

--- src/@clean/modules/project/usecases/get-project-usecase.ts
+++ src/@clean/modules/project/usecases/get-project-usecase.ts
@@ -1,6 +1,4 @@
 import { Project } from "../../../shared/domain/entities/project";
-import { User } from "../../../shared/domain/entities/user";
-import { SHIFT } from "../../../shared/domain/enums/shift-enum";
 import { IProjectRepository } from "../domain/repositories/project-repository-interface";
 
 export class GetProjectUsecase {
@@ -10,18 +8,8 @@ export class GetProjectUsecase {
         this._projectRepository = projectRepository;
     }
 
-    async execute(projectId: number, newTitle?: string,
-        newQualification?: string,
-        newCode?: string,
-        newShift?: SHIFT,
-        newStandNumber?: string,
-        newIsEntrepreneurship?: boolean,
-        newProfessors?: User[],
-        newStudents?: User[]): Promise<Project> {
-        const project = await this._projectRepository.updateProject(
-            projectId, newTitle, newQualification, newCode, newShift, newStandNumber, newIsEntrepreneurship, newProfessors, newStudents
-        );
-        
+    async execute(projectId: number): Promise<Project> {
+        const project = await this._projectRepository.getProject(projectId);
         return project;
     }
 }
