7a23c02c81b6376f8c33c5a7145299296773e8ff
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-02-16 22:29:33 -0300

    add ods page and logic to select then

--- src/app/view/pages/ProjectData/ProjectData.tsx
+++ src/app/view/pages/ProjectData/ProjectData.tsx
@@ -10,6 +10,8 @@ import isEqual from "lodash.isequal";
 import "./ProjectData.css";
 import CircularLoading from "../../components/CircularLoading/CircularLoading";
 import BasicButton from "../../components/BasicButton/BasicButton";
+import { odsList } from "../../../utils/ods-list";
+import DataCard from "../../components/DataCard/DataCard";
 
 export default function ProjectData() {
   // get the project id from the url to fetch the project data
@@ -27,6 +29,7 @@ export default function ProjectData() {
   const [projectDescription, setProjectDescription] = useState<string>(
     "Descrição do projeto muito legal e interessante."
   );
+  const [selectedOds, setSelectedOds] = useState<number[]>([]);
 
   useEffect(() => {
     if (isEqual(project, ProjectModel.empty())) {
@@ -44,61 +47,88 @@ export default function ProjectData() {
       <ReturnButton to={`/project/${projectId}`} />
       {isLoading && <CircularLoading />}
       {isSkeletonLoading ? null : (
-        <Card
-          headerTitleClassName="header__title--upper"
-          cardClassName="card--margin"
-          headerTitle={project.title}
-        >
-          <div className="card__main">
-            <div className="input">
-              <h2 className="input__title">Título: </h2>
-              <input
-                className="input__field"
-                value={projectTitle}
-                onChange={(value) => setProjectTitle(value.currentTarget.value)}
-              ></input>
-              <BasicButton
-                title="Salvar"
-                buttonClassName="input__btn"
-                onClick={() =>
-                  handleFetch(setIsLoading, showBoundary, updateProject(projectId, projectTitle))
-                }
-              ></BasicButton>
+        <>
+          {" "}
+          <Card
+            headerTitleClassName="header__title--upper"
+            cardClassName="card--margin"
+            headerTitle={project.title}
+          >
+            <div className="card__main">
+              <div className="input">
+                <h2 className="input__title">Título: </h2>
+                <input
+                  className="input__field"
+                  value={projectTitle}
+                  onChange={(value) => setProjectTitle(value.currentTarget.value)}
+                ></input>
+                <BasicButton
+                  title="Salvar"
+                  buttonClassName="input__btn"
+                  onClick={() =>
+                    handleFetch(setIsLoading, showBoundary, updateProject(projectId, projectTitle))
+                  }
+                ></BasicButton>
+              </div>
+              <div className="input input--bigger input--column">
+                <h2 className="input__title">Descrição: </h2>
+                <textarea
+                  className="input__field"
+                  value={projectDescription}
+                  onChange={(value) => setProjectDescription(value.currentTarget.value)}
+                ></textarea>
+                <BasicButton
+                  title="Salvar"
+                  buttonClassName="input__btn--end"
+                  onClick={() =>
+                    handleFetch(setIsLoading, showBoundary, updateProject(projectId, projectTitle))
+                  }
+                ></BasicButton>
+              </div>
+              <span className="card__span">
+                Os Objetivos de Desenvolvimento Sustentável (ODS) são uma agenda mundial adotada
+                durante a Cúpula das Nações Unidas sobre o Desenvolvimento Sustentável em setembro
+                de 2015 composta por 17 objetivos e 169 metas a serem atingidos até 2030. Para saber
+                mais veja a tela de{" "}
+                <Link
+                  className="card__span"
+                  target="_blank"
+                  to={"https://sistema-eureka.maua.br/downloads/arquivos/ODS-agenda2030-pt-br.pdf"}
+                >
+                  downloads
+                </Link>{" "}
+                ou{" "}
+                <Link className="card__span" target="_blank" to={"https://brasil.un.org/pt-br"}>
+                  link
+                </Link>
+              </span>
             </div>
-            <div className="input input--bigger input--column">
-              <h2 className="input__title">Descrição: </h2>
-              <textarea
-                className="input__field"
-                value={projectDescription}
-                onChange={(value) => setProjectDescription(value.currentTarget.value)}
-              ></textarea>
-              <BasicButton
-                title="Salvar"
-                buttonClassName="input__btn--end"
-                onClick={() =>
-                  handleFetch(setIsLoading, showBoundary, updateProject(projectId, projectTitle))
-                }
-              ></BasicButton>
+          </Card>
+          <Card headerTitle="Selecione quais ODS o trabalho está envolvido: ">
+            <div className="card__main--flex">
+              {odsList.map((ods) => {
+                return (
+                  <DataCard
+                    title={ods.title}
+                    description={ods.description}
+                    image={ods.image}
+                    key={ods.odsId}
+                    backgroundColor={
+                      selectedOds.includes(ods.odsId) ? "var(--dark-mustard)" : undefined
+                    }
+                    onClick={() => {
+                      setSelectedOds((previousOds) => {
+                        return previousOds.includes(ods.odsId)
+                          ? previousOds.filter((id) => id !== ods.odsId)
+                          : [...previousOds, ods.odsId];
+                      });
+                    }}
+                  />
+                );
+              })}
             </div>
-            <span className="card__span">
-              Os Objetivos de Desenvolvimento Sustentável (ODS) são uma agenda mundial adotada
-              durante a Cúpula das Nações Unidas sobre o Desenvolvimento Sustentável em setembro de
-              2015 composta por 17 objetivos e 169 metas a serem atingidos até 2030. Para saber mais
-              veja a tela de{" "}
-              <Link
-                className="card__span"
-                target="_blank"
-                to={"https://sistema-eureka.maua.br/downloads/arquivos/ODS-agenda2030-pt-br.pdf"}
-              >
-                downloads
-              </Link>{" "}
-              ou{" "}
-              <Link className="card__span" target="_blank" to={"https://brasil.un.org/pt-br"}>
-                link
-              </Link>
-            </span>
-          </div>
-        </Card>
+          </Card>
+        </>
       )}
     </main>
   );
