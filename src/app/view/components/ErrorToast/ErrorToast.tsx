69582c55ba9c34116adadc9fa275524a19cb6b58
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-02-19 16:55:47 -0300

    chage toast config

--- src/app/view/components/ErrorToast/ErrorToast.tsx
+++ src/app/view/components/ErrorToast/ErrorToast.tsx
@@ -15,6 +15,7 @@ export default function ErrorToast() {
       rtl={false}
       draggable={false}
       pauseOnHover={false}
+      pauseOnFocusLoss={false}
       theme="colored"
       transition={Flip}
     />
