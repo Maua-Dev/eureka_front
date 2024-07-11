bd78bd2d88fe6931ee74aae380d6eff9e68a0645
Sérgio Guidi Trovó <sergio.guidi.trovo@gmail.com>
2024-07-10 19:56:54 -0500

    adding abstract delivery

--- src/app/pages/AbstractPage/AbstractPage.tsx
+++ src/app/pages/AbstractPage/AbstractPage.tsx
@@ -11,24 +11,34 @@ import { ProjectModel } from "../../models/project-model";
 import { handleFetch } from "../../utils/functions/handle-fetch";
 import { toast } from "react-toastify";
 import DefaultButton from "../../ui/components/DefaultButton/DefaultButton";
+import { DeliveryContext } from "../../context/delivery-context";
+import { AbstractType } from "../../utils/@types/abstract-type";
+import { AuthContext } from "../../context/auth-context";
 
 export default function AbstractPage() {
   const [isSkeletonLoading, setIsSkeletonLoading] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const { projectFromContext, getProject, updateProject } = useContext(ProjectContext);
-
   const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
+  const [projectAbstract, setProjectAbstract] = useState<string>("");
   const [keywords, setKeywords] = useState<string>("");
 
   const [titleENG, setTitleENG] = useState<string>("");
-  const [descriptionENG, setDescriptionENG] = useState<string>("");
+  const [projectAbstractENG, setProjectAbstractENG] = useState<string>("");
   const [keywordsENG, setKeywordsENG] = useState<string>("");
 
   // get the project id from the url to fetch the project data
-  const { projectId } = useParams();
+  const { projectId, taskId } = useParams();
   const projectIdFromPath = parseInt(projectId!);
+  const taskIdFromPath = parseInt(taskId!);
+
+  const { projectFromContext, getProject, updateProject } = useContext(ProjectContext);
+  const { createDelivery, getDeliveries, deliveriesFromContext } = useContext(DeliveryContext);
+  const { userFromContext } = useContext(AuthContext);
+
+  const deliveryFromContext = deliveriesFromContext.find(
+    (delivery) => delivery.task.title === "Resumo/Abstract"
+  );
 
   // error boundary to catch errors in the components (used in handleFetch function)
   const { showBoundary } = useErrorBoundary();
@@ -37,11 +47,103 @@ export default function AbstractPage() {
     if (isEqual(projectFromContext, ProjectModel.empty())) {
       handleFetch(setIsSkeletonLoading, showBoundary, undefined, getProject(projectIdFromPath));
     }
+    if (isEqual(deliveriesFromContext, [])) {
+      handleFetch(setIsSkeletonLoading, showBoundary, undefined, getDeliveries(projectIdFromPath));
+    }
   }, []);
 
+  // Abstract PT
+
+  const abstractsFromContext = deliveryFromContext?.content["abstract"] as AbstractType;
+
   useEffect(() => {
-    setTitle(projectFromContext.title);
-  }, [projectFromContext.title]);
+    setAbstract(abstractsFromContext);
+  }, [abstractsFromContext]);
+
+  const [abstract, setAbstract] = useState<AbstractType>(abstractsFromContext);
+
+  useEffect(() => {
+    if (abstractsFromContext?.title != null && title !== abstractsFromContext.title) {
+      setTitle(abstractsFromContext.title);
+    }
+  }, [abstractsFromContext?.title != undefined]);
+
+  useEffect(() => {
+    if (
+      abstractsFromContext?.projectAbstract != null &&
+      projectAbstract !== abstractsFromContext.projectAbstract
+    ) {
+      setProjectAbstract(abstractsFromContext.projectAbstract);
+    }
+  }, [abstractsFromContext?.projectAbstract != undefined]);
+
+  useEffect(() => {
+    if (abstractsFromContext?.keyWords != null && keywords !== abstractsFromContext.keyWords) {
+      setKeywords(abstractsFromContext.keyWords);
+    }
+  }, [abstractsFromContext?.keyWords != undefined]);
+
+  useEffect(() => {
+    const updatedAbstract: {
+      title?: string;
+      projectAbstract?: string;
+      keyWords?: string;
+    } = {};
+
+    if (title !== "") {
+      updatedAbstract.title = title;
+    }
+
+    if (projectAbstract !== "") {
+      updatedAbstract.projectAbstract = projectAbstract;
+    }
+
+    if (keywords !== "") {
+      updatedAbstract.keyWords = keywords;
+    }
+
+    const newAbstract = {
+      title: updatedAbstract.title!,
+      projectAbstract: updatedAbstract.projectAbstract!,
+      keyWords: updatedAbstract.keyWords!,
+    };
+
+    setAbstract(newAbstract);
+  }, [title, projectAbstract, keywords]);
+
+  const handleSendButtonClick = () => {
+    let hasError = false;
+    if (isEqual(abstract, abstractsFromContext)) {
+      toast.error("O resumo não foi alterado");
+      hasError = true;
+    }
+
+    if (
+      abstract.title == "" ||
+      abstract.title == undefined ||
+      abstract.projectAbstract == "" ||
+      abstract.projectAbstract == undefined ||
+      abstract.keyWords == "" ||
+      abstract.keyWords == undefined
+    ) {
+      console.log(abstract);
+      console.log(abstract.title);
+      toast.error("Preencha os campos de título e/ou resumo e/ou palavras chave");
+      hasError = true;
+    }
+
+    if (!hasError) {
+      handleFetch(
+        setIsLoading,
+        showBoundary,
+        undefined,
+        createDelivery(taskIdFromPath, projectIdFromPath, userFromContext.userId, {
+          abstract: abstract,
+        })
+      );
+      toast.success("Resumo enviado com sucesso");
+    }
+  };
 
   return (
     <main className="abstract_page">
@@ -90,8 +192,8 @@ export default function AbstractPage() {
               </div>
               <div>
                 <DefaultTextField
-                  setValue={setDescription}
-                  value={description}
+                  setValue={setProjectAbstract}
+                  value={projectAbstract}
                   type="text"
                   topTitle="Resumo do trabalho"
                   isSaveButtonIncluded={false}
@@ -117,7 +219,11 @@ export default function AbstractPage() {
                 </span>
               </div>
               <div>
-                <DefaultButton title="Enviar" buttonClassName="box__btn--end" />
+                <DefaultButton
+                  title="Enviar"
+                  buttonClassName="box__btn--end"
+                  onClick={handleSendButtonClick}
+                />
               </div>
             </div>
           </HeaderedBox>
@@ -160,8 +266,8 @@ export default function AbstractPage() {
               </div>
               <div>
                 <DefaultTextField
-                  setValue={setDescriptionENG}
-                  value={descriptionENG}
+                  setValue={setProjectAbstractENG}
+                  value={projectAbstractENG}
                   type="text"
                   topTitle="Resumo do trabalho (em inglês)"
                   isSaveButtonIncluded={false}
