11d471fb6804d13d150c606426fcf6b6df20f858
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-07-22 19:30:45 -0300

    optimizing project with code sppliting, swc config, image and code compression losing 5% quality in maximum

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import imagemin from "vite-plugin-imagemin";
+import analyze from "rollup-plugin-analyzer";
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react({
             devTarget: "es2022"
+        }),
+        imagemin({
+            optipng: {
+                optimizationLevel: 7
+            },
+            pngquant: {
+                quality: [0.95, 1.0],
+                speed: 1
+            },
+            verbose: true,
+            svgo: {
+                plugins: [
+                    {
+                        name: "removeViewBox"
+                    },
+                    {
+                        name: "removeEmptyAttrs",
+                        active: false
+                    }
+                ]
+            }
+        }),
+        analyze({
+            summaryOnly: true
+        }),
+        compression({
+            algorithm: "brotliCompress",
+            ext: ".br"
         })
     ],
     resolve: {
@@ -31,5 +62,9 @@ export default defineConfig({
             "@@types": "/src/app/utils/@types"
         }
     },
-    envPrefix: "APP_"
+    envPrefix: "APP_",
+    optimizeDeps: {
+        exclude: ["react-spinners"]
+    },
+    cacheDir: "node_modules/.vite_cache"
 });
